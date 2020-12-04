a2bbfcb25940aa4a0540a25e4e1190cd2a7dc32c
Pierre <iwaongit@gmail.com>
2020-12-04 16:02:35 +0100

    add index file with app route

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import bearerToken from 'express-bearer-token';
+import shell from 'shelljs';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const TOKEN = process.env.TOKEN;
+
+const app = express();
+const port = process.env.PORT || 8080;
+app.use(express.json());
+app.use(bearerToken());
+
+app.get('/app/:app', async (req, res) => {
+    if(req.token) {
+        if(req.token === TOKEN) {
+            let pid = shell.cat(`${process.env.HOME}/.pm2/pids/${req.params.app}*.pid`);
+            if(pid.stderr)
+                return res.sendStatus(404)
+            return res.sendStatus(200)
+        }
+    }
+    return res.sendStatus(403)
+});
+
+app.use((req, res) => {
+    res.sendStatus(404);
+});
+
+app.listen(port, () => {
+    console.log(`server started at http://localhost:${port}`);
+});
